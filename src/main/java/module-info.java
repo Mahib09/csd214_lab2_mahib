320ac04b3697f5f9320a4a249b1c68c5b358d10e
Mahib09 <Mahib.Ranamagar@triosstudent.com>
2024-02-21 16:27:40 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.lab2test {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                                requires com.almasb.fxgl.all;
+    
+    opens com.example.csd214_lab2_mahib to javafx.fxml;
+    exports com.example.csd214_lab2_mahib;
+}
